3959c7b27d6aa587f71f1e7f674c9f3648501694
Andrii Chubenko <stalkerock@gmail.com>
2025-03-22 23:42:08 +0200

    add post, avatar, comment, delete

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './Post.css';
+import { Avatar, Button } from '@mui/material';
 
 const BASE_URL = 'http://localhost:8000/'
 
 function Post({post}) {
 
     const [imageUrl, setImageUrl] = useState('');
+    const [comments, setComments] = useState([]);
 
     useEffect(() => {
         if (post.image_url_type === 'absolute') {
@@ -15,11 +17,38 @@ function Post({post}) {
         }
     }, [post.image_url, post.image_url_type])
 
+    useEffect(() => {
+        setComments(post.comments);
+    },[post.comments])
+
     console.log("Image URL: ", imageUrl);
 
     return (
         <div className='post'>
+            <div>
+                <Avatar
+                    alt="Andrii"
+                    src=""/>
+                    <div className='post_headerInfo'>
+                    <h3>
+                    {post.user.username}  
+                    </h3>
+                    <Button className='post_delete'>Delete</Button>
+                    </div>
+                <h3>{post.username}</h3>
+            </div>
             <img className='post_image' src={imageUrl} alt="Post" />
+            <h4 className='post_text'>{post.caption}</h4>
+
+            <div className='post_comments'>
+                {
+                    comments.map(comment => (
+                        <p>
+                            <strong>{comment.username}:</strong>{comment.text}
+                        </p>
+                    ))
+                }
+            </div>
             </div>
     )
 }
